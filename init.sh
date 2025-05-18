68df31f00bc631184b2de4ccbf0984103fe03674
cnvy <143577933+cnvy0@users.noreply.github.com>
2025-05-18 01:00:41 -0700

    fix small thing

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@
 sh createvirtualmic.sh
 source ./venv/bin/activate
 echo "CTRL + C to stop server"
-echo "Virtual PulseAudio device will persist until reboot."
+echo "Virtual audio device will persist until reboot."
 echo "--------------------------------------------------"
 python3 server.py
